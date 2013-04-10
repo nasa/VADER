03a09fd9f7c2b0c51d9160449e7432c7775d5564
Peter Clark Mosbacher <peter.c.mosbacher@nasa.gov>
2013-04-10 06:58:10 -0500

    Added Switch Status to Mission Control
    Mission Control now reads status from the Kramer switch  and sends the
    data as a dictionary in response to an HTTP GET.

--- service.mission.control/service.py
+++ service.mission.control/service.py
@@ -8,6 +8,7 @@ from threading import Thread
 import xbmc
 import xbmcaddon
 import xbmcgui
+import binascii
 
 if sys.version_info < (2, 7):
     import simplejson
@@ -33,10 +34,10 @@ class SwitchStatus(SimpleHTTPServer.SimpleHTTPRequestHandler):
     
     def do_GET(self):
         if self.path == '/':
-            print >>self.wfile, "<html><body><a href='/run'>Patient Test</a>" + str(theCounter) + "</body></html>"
+            print >>self.wfile, "<html><body>" + str(theCounter) + "<a href='/run'>Patient Test</a>" + str(theStatus) + "</body></html>"
         if self.path == '/run':
             print 'Patience is a virtue...'
-            print >>self.wfile, "<html><body><a href='/'>Impatient Test</a>" + str(theCounter) + "</body></html>"
+            print >>self.wfile, "<html><body>" + str(theCounter) + "<a href='/'>Impatient Test</a>" + str(theStatus) + "</body></html>"
 
 def startServing(server):
     print "begin serving requests"
@@ -45,6 +46,7 @@ def startServing(server):
 if (__name__ == "__main__"):
     xbmc.log('Version %s started' % __addonversion__)
     theCounter = 0
+    theStatus = {'left': 1, 'center1': 1, 'center2': 2, 'right1': 1, 'right2':2, 'actionCenter': 3}
     httpd = SocketServer.TCPServer(('', PORT), SwitchStatus)
     print "serving at port", PORT
     Thread(target=startServing, args=(httpd,)).start()
@@ -52,6 +54,67 @@ if (__name__ == "__main__"):
     while (not xbmc.abortRequested):
         time.sleep(2.5)
         theCounter += 1
+        
+        # This is where the serial stuff begins
+        ser = serial.Serial(2, 9600, timeout=1)
+        ser.flushInput()
+        ser.write('\x05\x80\x81\x81')
+        ser.read(2)
+        out = ser.read()
+        ser.close()
+        foo = binascii.b2a_qp(out)
+        source = foo[2]
+        theStatus['left'] = source
+        
+        ser = serial.Serial(2, 9600, timeout=1)
+        ser.flushInput()
+        ser.write('\x05\x80\x82\x81')
+        ser.read(2)
+        out = ser.read()
+        ser.close()
+        foo = binascii.b2a_qp(out)
+        source = foo[2]
+        theStatus['center1'] = source
+        
+        ser = serial.Serial(2, 9600, timeout=1)
+        ser.flushInput()
+        ser.write('\x05\x80\x83\x81')
+        ser.read(2)
+        out = ser.read()
+        ser.close()
+        foo = binascii.b2a_qp(out)
+        source = foo[2]
+        theStatus['center2'] = source
+        
+        ser = serial.Serial(2, 9600, timeout=1)
+        ser.flushInput()
+        ser.write('\x05\x80\x84\x81')
+        ser.read(2)
+        out = ser.read()
+        ser.close()
+        foo = binascii.b2a_qp(out)
+        source = foo[2]
+        theStatus['right1'] = source
+        
+        ser = serial.Serial(2, 9600, timeout=1)
+        ser.flushInput()
+        ser.write('\x05\x80\x85\x81')
+        ser.read(2)
+        out = ser.read()
+        ser.close()
+        foo = binascii.b2a_qp(out)
+        source = foo[2]
+        theStatus['right2'] = source
+        
+        ser = serial.Serial(2, 9600, timeout=1)
+        ser.flushInput()
+        ser.write('\x05\x80\x86\x81')
+        ser.read(2)
+        out = ser.read()
+        ser.close()
+        foo = binascii.b2a_qp(out)
+        source = foo[2]
+        theStatus['actionCenter'] = source
     print "starting server shutdown"
     httpd.shutdown()
     print "finished server shutdown"
